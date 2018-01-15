ef9151682556c90a902cc9ae6409d8ce3dbee4fd
Sebastian Källstedt <sk223dn@student.lnu.se>
2018-01-15 11:21:12 +0100

    Init update

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,1 @@
 'use strict'
-const Desktop = require('./Desktop')
-
-/**
- * Initiates the whole application by creating a new Desktop and init this.
- */
-let newDesktop = new Desktop()
-newDesktop.init()
